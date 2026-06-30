71edb972eafddb40f8a8b0f4e317cb7c7d251c30
Sumit-do <b240062@nitsikkim.ac.in>
2026-06-30 20:45:05 +0530

    Add RemovevomeR [C] from Codeforces

---
+++ 2241/C - RemovevomeR/solution.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+void solve(){
+    int n; cin>>n;
+    string s; cin>>s;
+    bool h01=0, h10=0;
+    for(int i=0;i<n-1;i++){
+        if(s[i]=='0'&&s[i+1]=='1') h01=1;
+        if(s[i]=='1'&&s[i+1]=='0') h10=1;
+    }
+    int minwf=(h01^h10)?2:1;
+    cout<<minwf<<"
+";
+}
+int main(){
+    int t; cin>>t;
+    while(t--) solve();
+}
