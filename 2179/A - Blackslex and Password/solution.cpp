bb8e59a8aa6ee3e7017729a6a55996bf5acaf4f1
Sumit-do <b240062@nitsikkim.ac.in>
2026-07-04 17:48:12 +0530

    Add Blackslex and Password [A] from Codeforces

---
+++ 2179/A - Blackslex and Password/solution.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define ll long long
+#define fr(i,n) for(int i=0;i<n;i++)
+ 
+ 
+void solve(){
+    int n,k;
+    cin>>n>>k;
+    cout<<n*k+1<<endl;
+}
+ 
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin>> t;
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
