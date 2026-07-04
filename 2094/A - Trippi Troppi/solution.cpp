e809a102c3ad05a5fbd1ac97e91316cf7103d808
Sumit-do <b240062@nitsikkim.ac.in>
2026-07-04 17:19:50 +0530

    Add Trippi Troppi [A] from Codeforces

---
+++ 2094/A - Trippi Troppi/solution.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        string s[3];
+        cin>>s[0];
+        cin>>s[1];
+        cin>>s[2];
+        cout<<s[0][0]<<s[1][0]<<s[2][0]<<endl;
+ 
+    }
+    return 0;
+}
