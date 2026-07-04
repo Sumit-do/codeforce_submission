dd89408856115032615e4b06180f6f9f4f347cab
Sumit-do <b240062@nitsikkim.ac.in>
2026-07-04 17:24:06 +0530

    Add Remove a Progression [A] from Codeforces

---
+++ 1194/A - Remove a Progression/solution.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define fr(i,n) for(int i=0;i<n;i++)
+void solve() {
+    int x ,n;
+    cin>>n>>x;
+    cout<<(2*x)<<endl;
+}
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
