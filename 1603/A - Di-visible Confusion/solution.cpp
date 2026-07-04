236db72ea3bd212c960322a7e4fa8b9b57b56152
Sumit-do <b240062@nitsikkim.ac.in>
2026-07-04 18:00:57 +0530

    Add Di-visible Confusion [A] from Codeforces

---
+++ 1603/A - Di-visible Confusion/solution.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define fast_io ios::sync_with_stdio(false); cin.tie(NULL);
+ 
+#define ll long long
+#define fr(i,n) for(int i=0;i<n;i++)
+ 
+long long f(long long curr,long long x){
+    //if(curr%x==0)return curr;
+    if( curr > INT32_MAX)return curr;
+    return (curr/__gcd(curr,x))*x;
+}
+ 
+void solve(){
+    int n;
+    cin>>n;
+    vector<int> vec(n);
+    fr(i,n)cin>>vec[i];
+    long long test = 2;
+    fr(i,n){
+        if(vec[i]%test==0)return void(cout<<"NO
+");
+        test=f(test,i+3);
+        //else break;
+    }
+    cout<<"YES
+";
+}
+ 
+ 
+ 
+ 
+int main(){
+    int t;
+    cin >> t;
+    while (t--) solve();
+    return 0;
+}
