b5a4f996969ccfb71a7a651457d71e5e361dcc31
Sumit-do <b240062@nitsikkim.ac.in>
2026-07-04 17:27:23 +0530

    Add Longest Divisors Interval [B] from Codeforces

---
+++ 1855/B - Longest Divisors Interval/solution.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <numeric>
+#include<bits/stdc++.h>
+using namespace std;
+#define   fr(i,n) for(int i=0;i<n;i++)
+#define ll long long int
+ 
+bool isprime(ll a){
+    for(ll i=2;i*i<=a;i++){
+        if(a%i==0) return false;
+    }
+    return true;
+}
+ 
+ll lcm(ll a ,ll b){
+    return (a*b)/(__gcd(a,b));
+}
+ 
+void solve(){
+   ll n;
+   cin>>n;
+   int i=1;
+   int ans =0;
+   while(n%i==0){
+    i++;
+    ans++;
+   }
+   cout<<ans<<endl;
+}
+ 
+ 
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    int t;
+    std::cin >> t;
+    //t=1;
+    
+    while(t--) solve();
+ 
+    return 0;
+}
