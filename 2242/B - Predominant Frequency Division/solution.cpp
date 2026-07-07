22d38d25ac794742b02f1ae52b306d2666ebc88a
Sumit-do <b240062@nitsikkim.ac.in>
2026-07-07 08:20:01 +0530

    Add Predominant Frequency Division [B] from Codeforces

--- 2242/B - Predominant Frequency Division/solution.cpp
+++ 2242/B - Predominant Frequency Division/solution.cpp
@@ -1,46 +1,71 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
  
 void solve() {
     int n;
     cin >> n;
-    vector<int> a(n);
-    for (int &x : a) cin >> x;
  
-    vector<int> p1(n), p2(n);
-    int c1 = 0, c2 = 0;
-    for (int i = 0; i < n; ++i) {
-        c1 += (a[i] == 1 ? 1 : -1);
-        p1[i] = c1;
-        c2 += (a[i] <= 2 ? 1 : -1);
-        p2[i] = c2;
-    }
+    vector<int> v(n);
+    for (auto &x : v) cin >> x;
  
-    vector<int> mx(n);
-    if (n > 2) {
-        mx[n - 2] = p2[n - 2];
-        for (int i = n - 3; i >= 0; --i) {
-            mx[i] = max(p2[i], mx[i + 1]);
+    int count1 = 0;
+    int count23 = 0;
+    int count12 = 0;
+    int count3 = 0;
+ 
+    bool found = false;
+    bool found2 = false;
+    int number = -1;
+ 
+    for (int i = 0; i < n; i++) {
+        if (!found) {
+            if (v[i] == 2 || v[i] == 3) {
+                count23++;
+            } else {
+                count1++;
+ 
+                if (count1 > count23) {
+                    found = true;
+ 
+                    if (i + 1 < n && v[i + 1] == 3)
+                        i++;
+                }
+                else if (count1 == count23) {
+                    found = true;
+                }
+            }
+        }
+        else if (!found2) {
+            if (v[i] == 3) {
+                count3++;
+            }
+            else {
+                count12++;
+ 
+                if (count12 >= count3) {
+                    found2 = true;
+                    number = i;
+                    break;
+                }
+            }
         }
     }
  
-    for (int i = 0; i < n - 2; ++i) {
-        if (p1[i] >= 0 && mx[i + 1] >= p2[i]) {
-            cout << "YES
+    if (found && found2 && number != n - 1)
+        cout << "YES
 ";
-            return;
-        }
-    }
-    cout << "NO
+    else
+        cout << "NO
 ";
 }
  
 int main() {
+ 
     int t;
-    if (cin >> t) {
-        while (t--) {
-            solve();
-        }
+    cin >> t;
+    while (t--) {
+        solve();
     }
+ 
     return 0;
 }
